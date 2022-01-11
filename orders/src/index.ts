fc969a7ea1b3bbc4aa8d1800b85f11ad7fb8198a
sfrumkin <sfrumkin@gmail.com>
2022-01-11 08:25:36 +0200

    change startup script

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -5,7 +5,7 @@ import { TicketCreatedListener } from './events/listeners/ticket-created-listene
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 const start = async () => {
-  console.log('Starting ...');
+  console.log('Starting !!!');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
